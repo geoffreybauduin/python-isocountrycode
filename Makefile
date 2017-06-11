d4d06cf575d36c9f40444652797150942d9ebe99
Geoffrey Bauduin <bauduin.geo@gmail.com>
2017-06-11 21:18:20 +0200

    First version

---
+++ Makefile
@@ -0,0 +1,4 @@
+publish:
+	python setup.py register -r pypi
+	python setup.py sdist upload -r pypi
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+ISO Country Codes
+----------------
+
+Provides enums for ISO Country Codes
+"""
+from setuptools import setup
+
+setup(
+    name='isocountrycode',
+    version='1.0',
+    url='http://github.com/geoffreybauduin/python-isocountrycode',
+    license='MIT',
+    author='Geoffrey Bauduin',
+    author_email='bauduin.geo@gmail.com',
+    maintainer='Geoffrey Bauduin',
+    maintainer_email='bauduin.geo@gmail.com',
+    description="Provides enums for ISO representation of countries",
+    long_description=__doc__,
+    packages=['isocountrycode'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        "enum==0.4.6",
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
